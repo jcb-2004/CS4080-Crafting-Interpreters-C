140e14bb69cf02285f5eb98ef4dc23e7caee1a22
jcb-2004 <joshuaboucher2004@gmail.com>
2026-03-18 16:52:42 -0700

    Chapter 14 additions

--- c/main.c
+++ c/main.c
@@ -1,9 +1,18 @@
-#include <stdio.h>
+#include "common.h"
+#include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
-    printf("argc = %d\n", argc);
-    for (int i = 0; i < argc; i++) {
-        printf("argv[%d] = %s\n", i, argv[i]);
-    }
-    return 0;
+  Chunk chunk;
+  initChunk(&chunk);
+	
+  int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
+
+  disassembleChunk(&chunk, "test chunk");
+  freeChunk(&chunk);
+  return 0;
 }
