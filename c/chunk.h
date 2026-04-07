deb68a8b7f2a57c33d0d1369031c902eba0ff4fa
jcb-2004 <joshuaboucher2004@gmail.com>
2026-04-07 16:59:43 -0700

    Chapter 18 additions

--- c/chunk.h
+++ c/chunk.h
@@ -10,6 +10,13 @@ typedef enum {
   OP_MULTIPLY,
   OP_DIVIDE,
   OP_CONSTANT,
+  OP_NIL,
+  OP_TRUE,
+  OP_FALSE,
+  OP_EQUAL,
+  OP_GREATER,
+  OP_LESS,
+  OP_NOT,
   OP_NEGATE,
   OP_RETURN,
 } OpCode;
