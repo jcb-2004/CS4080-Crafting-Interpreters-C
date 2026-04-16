bd35cbcc88cfe6694fb619abaccd7c624573b280
jcb-2004 <joshuaboucher2004@gmail.com>
2026-04-15 18:16:11 -0700

    Chapter 20 Challenge 3

--- c/main.c
+++ c/main.c
@@ -1,74 +1,131 @@
+//Chapter 20 Challenge 3
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
 #include "common.h"
-#include "chunk.h"
-#include "debug.h"
+#include "memory.h"
+#include "object.h"
+#include "table.h"
+#include "value.h"
 #include "vm.h"
 
-static void repl() {
-  char line[1024];
-  for (;;) {
-    printf("> ");
+static double nowSeconds(void) {
+  return (double)clock() / CLOCKS_PER_SEC;
+}
 
-    if (!fgets(line, sizeof(line), stdin)) {
-      printf("\n");
-      break;
+static ObjString* makeKey(const char* prefix, int i) {
+  char buffer[64];
+  int length = snprintf(buffer, sizeof(buffer), "%s%d", prefix, i);
+  return copyString(buffer, length);
+}
+
+static ObjString** makeKeyArray(const char* prefix, int count) {
+  ObjString** keys = (ObjString**)malloc(sizeof(ObjString*) * count);
+  if (keys == NULL) {
+    fprintf(stderr, "Out of memory allocating keys.\n");
+    exit(1);
+  }
+
+  for (int i = 0; i < count; i++) {
+    keys[i] = makeKey(prefix, i);
+  }
+
+  return keys;
+}
+
+static void benchInsert(int count) {
+  Table table;
+  initTable(&table);
+
+  ObjString** keys = makeKeyArray("insertKey", count);
+
+  double start = nowSeconds();
+
+  for (int i = 0; i < count; i++) {
+    tableSet(&table, keys[i], NUMBER_VAL(i));
+  }
+
+  double end = nowSeconds();
+
+  printf("insert %d keys: %.6f sec\n", count, end - start);
+
+  free(keys);
+  freeTable(&table);
+}
+
+static void benchLookup(int count, int rounds) {
+  Table table;
+  initTable(&table);
+
+  ObjString** keys = makeKeyArray("lookupKey", count);
+
+  for (int i = 0; i < count; i++) {
+    tableSet(&table, keys[i], NUMBER_VAL(i));
+  }
+
+  Value value;
+  int hits = 0;
+  double start = nowSeconds();
+
+  for (int r = 0; r < rounds; r++) {
+    for (int i = 0; i < count; i++) {
+      if (tableGet(&table, keys[i], &value)) {
+        hits++;
+      }
     }
-
-    interpret(line);
   }
+
+  double end = nowSeconds();
+
+  printf("lookup %d keys x %d rounds: %.6f sec, number of hits: %d\n",
+       count, rounds, end - start, hits);
+
+  free(keys);
+  freeTable(&table);
 }
 
-static char* readFile(const char* path) {
-  FILE* file = fopen(path, "rb");
-  if (file == NULL) {
-    fprintf(stderr, "Could not open file \"%s\".\n", path);
-    exit(74);
+static void benchDeleteReinsert(int count) {
+  Table table;
+  initTable(&table);
+
+  ObjString** originalKeys = makeKeyArray("deleteKey", count);
+  ObjString** newKeys = makeKeyArray("newKey", count / 2);
+
+  for (int i = 0; i < count; i++) {
+    tableSet(&table, originalKeys[i], NUMBER_VAL(i));
   }
 
-  fseek(file, 0L, SEEK_END);
-  size_t fileSize = ftell(file);
-  rewind(file);
+  double start = nowSeconds();
 
-  char* buffer = (char*)malloc(fileSize + 1);
-  if (buffer == NULL) {
-    fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
-    exit(74);
+  for (int i = 0; i < count; i += 2) {
+    tableDelete(&table, originalKeys[i]);
   }
-	
-  size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
-  if (bytesRead < fileSize) {
-    fprintf(stderr, "Could not read file \"%s\".\n", path);
-    exit(74);
+
+  for (int i = 0; i < count / 2; i++) {
+    tableSet(&table, newKeys[i], NUMBER_VAL(count + i));
   }
-	
-  buffer[bytesRead] = '\0';
 
-  fclose(file);
-  return buffer;
-}
+  double end = nowSeconds();
 
-static void runFile(const char* path) {
-  char* source = readFile(path);
-  InterpretResult result = interpret(source);
-  free(source); 
+  printf("delete half + reinsert half (%d total start): %.6f sec\n",
+         count, end - start);
 
-  if (result == INTERPRET_COMPILE_ERROR) exit(65);
-  if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+  free(originalKeys);
+  free(newKeys);
+  freeTable(&table);
 }
 
 int main(int argc, const char* argv[]) {
+  (void)argc;
+  (void)argv;
+
   initVM();
-	
-  if (argc == 1) {
-    repl();
-  } else if (argc == 2) {
-    runFile(argv[1]);
-  } else {
-    fprintf(stderr, "Usage: clox [path]\n");
-    exit(64);
-  }
+
+  benchInsert(50000);
+  benchLookup(10000, 200);
+  benchDeleteReinsert(50000);
+
   freeVM();
   return 0;
 }
