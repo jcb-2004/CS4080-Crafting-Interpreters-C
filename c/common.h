cbc46544ea8fdda966e56f86bd4a362c19473a70
jcb-2004 <joshuaboucher2004@gmail.com>
2026-04-30 16:50:39 -0700

    Chapter 26 Additions

--- c/common.h
+++ c/common.h
@@ -9,6 +9,12 @@
 //#define DEBUG_PRINT_CODE
 //#define DEBUG_TRACE_EXECUTION
 
+//Comment out this line to disable garbage collector stress debug mode - it makes the GC run every time it possibly can to sniff out bugs
+//#define DEBUG_STRESS_GC
+
+//Comment out this line to disable garbage collector logging/debugging - it makes all dynamic memory modifications print to console
+//#define DEBUG_LOG_GC
+
 #define UINT8_COUNT (UINT8_MAX + 1)
 
 #endif
